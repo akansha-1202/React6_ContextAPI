ecce649e584557cd1ff8a34b345c7094d9ad8b79
akansha-1202 <akanshav325@gmail.com>
2023-06-09 20:45:56 +0530

    handson6

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RoutingFiles } from './Component/RoutingFiles'
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <RoutingFiles/>
+    </>
+  )
+}
+export default App
